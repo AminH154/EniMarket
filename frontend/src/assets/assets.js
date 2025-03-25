a86fb5c164290914961e73b85c28bf3f8b1c597b
amin <aminbouallegui0@gmail.com>
2025-03-25 18:34:02 +0100

    commit modif

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -17,3 +17,46 @@ import github from './images/github.png';
 import header from './images/header.png';
 import idee from './images/idee.png';
 import linkedin from './images/linkedin.png';
+import logo from './images/logo.png';
+import logout_icon from './images/logout_icon.png';
+import recharge from './images/recharge.png';
+import se_deconnecter from './images/se-deconnecter.png';
+import search_icon from './images/search_icon.png';
+import search1_icon from './images/search1-icon.png';
+import seconnecte   from './images/seconnecte.png';
+import sms  from './images/sms.png';
+import telephone_portable from './images/telephone-portable.png';
+import twitter_icon from './images/twitter_icon.png';
+import utilisateur from './images/utilisateur.png';
+
+export const assets = {
+    add_icon_green,
+    add_icon_white,
+    app_store,
+    appel,
+    avance_rapide,
+    bag_icon,
+    basker_icon,
+    bouclier,
+    commercialisation,
+    cross_icon,
+    depose,
+    facebook_icon,
+    facebook,
+    filtre,
+    flech_gauche,
+    github,
+    header,
+    idee,
+    linkedin,
+    logo,
+    logout_icon,
+    recharge,
+    se_deconnecter,
+    search_icon,
+    search1_icon,
+    seconnecte,
+    sms,
+    telephone_portable,
+    twitter_icon,
+    utilisateur,}
