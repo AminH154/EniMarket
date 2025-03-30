2ceda005d3b7066d82753a7d44a69debecdb669a
amin <aminbouallegui0@gmail.com>
2025-03-30 04:41:23 +0200

    commit

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -42,6 +42,7 @@ import utilisateur from "./utilisateur.png";
 import reset from "./reset.png";
 import s1 from "./s1.jpg";
 import s2 from "./s2.jpg";
+import img6 from "./img6.jpg"
 import s3 from "./s3.jpg";
 import s4 from "./s4.jpg";
 import s5 from "./s5.jpg";
@@ -50,6 +51,7 @@ import connects from "./connectes.jpg"
 export const swiper_assets = [R, s1, s2, s3, s4, s5];
 
 export const assets = {
+  img6,
   img5,
   connects,
   image3,
--- frontend/src/components/Info/Info.jsx
+++ frontend/src/components/Info/Info.jsx
@@ -1,42 +1,31 @@
-import React from 'react'
-import './Info.css'
-import { assets } from '../../assets/assets'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faArrowRight } from '@fortawesome/free-solid-svg-icons'; // Import des icônes
+import React from "react";
+import "./Info.css";
+import { assets } from "../../assets/assets";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faArrowRight } from "@fortawesome/free-solid-svg-icons"; // Import des icônes
 
 const Info = () => {
   return (
-    <div className='Images'>
-        <div className="ImageRight">
-            <h1>Vous avez quelque chose à vendre?</h1>
-            <button>
-                publier une annonce
-            </button>
-            <img src={assets.image3} alt="" className='img1' >
-            </img>
-        </div>
+    <div className="Images">
+      <div className="ImageRight">
+        <h1>Vous avez quelque chose à vendre?</h1>
+        <button>publier une annonce</button>
+        <img src={assets.image3} alt="" className="img1"></img>
+      </div>
       <div className="derection">
-      <div className="ImgLeft">
-        <h1>connecte</h1>
-            <button>
+        <div className="ImgLeft">
+          <h1>connecter</h1>
+          <button>
             <FontAwesomeIcon icon={faArrowRight} /> Cliquez ici
-            </button> 
-             
-            <img src={assets.img5} alt="" />
-            
+          </button>
+          <img src={assets.img5} alt="" />
         </div>
         <div className="ImgButtom">
-            <img src={assets.annonce} alt="" />
+          <img src={assets.img6} alt="" />
         </div>
-
-
       </div>
-      
-    
-       
-      
     </div>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
