aef042b497637e09049a68d7cf54266179568aa8
amin <aminbouallegui0@gmail.com>
2025-04-01 23:09:42 +0200

    commit

--- frontend/src/pages/FiltreAvance/FiltreAvance.jsx
+++ frontend/src/pages/FiltreAvance/FiltreAvance.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import "./FiltreAvance.css";
 import { useParams } from "react-router-dom";
+import Filtre from "../../components/Filtre/Filtre"; // Adjust the path as needed
 
 const FiltreAvance = () => {
   const { category } = useParams(); // Extract category from URL
 
   return (
     <div className="FiltreAvance">
-      <h1>Filtre Avancé</h1>
-      {category ? (
-        <p>Selected Category: {category}</p>
-      ) : (
-        <p>No category selected</p>
-      )}
+     <div className="FiltreAvance_left">
+     <Filtre/>
+     </div>
+     <div className="FiltreAvance_right">
+
+     </div>
     </div>
   );
 };
